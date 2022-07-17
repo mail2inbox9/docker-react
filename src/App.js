bd694b71ba00d7cb0de05fb21734482a3f9b207a
root <mail2inbox9@gmail.com>
2022-07-17 15:46:14 +0000

    app.js updated with custom message

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          i was chnages to feature branch 
+          Learn React
         </a>
       </header>
     </div>
